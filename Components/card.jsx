7a73c8b32e37e8e15f4957399772151fbdb754ff
Naoki0509 <70335387+Naoki0509@users.noreply.github.com>
2021-08-07 13:49:30 +0900

    🔥

--- Components/card.jsx
+++ Components/card.jsx
@@ -1,25 +1,25 @@
-import { Box } from "@chakra-ui/layout";
+import { Box, Wrap } from "@chakra-ui/layout";
+import { Image } from "@chakra-ui/image";
+import { Badge } from "@chakra-ui/layout";
 
 const ITEMS = [
 	{ taitle: "HTML", src: "/HTML.png", alt: "html" },
 	{ taitle: "CSS", src: "/CSS.png", alt: "css" },
 	{ taitle: "Javascript", src: "/JS.png", alt: "JavaScript" },
-	{ taitle: "Reat", src: "/React.png", alt: "React" },
+	{ taitle: "React", src: "/React.png", alt: "React" },
 	{ taitle: "Next.js", src: "/Next.png", alt: "Next.js" },
 	{ taitle: "Tailwind CSS", src: "/Taileind.png", alt: "TailwindCss" },
 ];
 export function Card() {
 	return (
 		<div className="w-full bg-gray-300 ">
-			<Box />
-
-			{/* <h1 className="text-5xl font-serif font-bold">Skill Set</h1>
-			<p>HTML</p>
-			<p>CSS</p>
-			<p>Javascript</p>
-			<p>Reat</p>
-			<p>Next.js</p>
-			<p>Tailwind CSS</p> */}
+			{ITEMS.map((item) => {
+				return (
+					<Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
+						<Image src={item.src} alt={item.alt} boxsize="80px" />
+					</Box>
+				);
+			})}
 		</div>
 	);
 }
