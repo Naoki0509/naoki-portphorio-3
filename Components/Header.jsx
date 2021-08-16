c1c2931da4361a738cf67240d8e2f3611d5f4d1d
Naoki0509 <70335387+Naoki0509@users.noreply.github.com>
2021-08-16 20:23:49 +0900

    💡

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -2,25 +2,27 @@ import Link from "next/link";
 
 export function Header() {
 	return (
-		<header className="w-full flex flex-row">
-			<div className="pl-3 font-serif pr-32  text-4xl font-black">N's Blog</div>
-			<ul className="flex flex-row font-serif text-3xl">
-				<li className="px-8">
-					<Link href="/">
-						<a>Home</a>
-					</Link>
-				</li>
-				<li className="px-8">
-					<Link href="/about">
-						<a>About</a>
-					</Link>
-				</li>
-				<li className="px-8">
-					<Link href="/blog">
-						<a>Blog</a>
-					</Link>
-				</li>
-			</ul>
+		<header className="w-full bg-gray-100">
+			<nav className="flex justify-center">
+				<div className="pl-3 font-serif text-4xl font-black">N's Blog</div>
+				<ul className="flex flex-row font-serif text-3xl">
+					<li className="px-8">
+						<Link href="/">
+							<a>Home</a>
+						</Link>
+					</li>
+					<li className="px-8">
+						<Link href="/about">
+							<a>About</a>
+						</Link>
+					</li>
+					<li className="px-8">
+						<Link href="/blog">
+							<a>Blog</a>
+						</Link>
+					</li>
+				</ul>
+			</nav>
 		</header>
 	);
 }
